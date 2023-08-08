20a30d68ec033470d5a3cedba74da032736ef0e3
Clae <56447997+clayruh@users.noreply.github.com>
2023-08-08 15:32:12 -0400

    first two deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+
+  // STATE
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function addNewToy(newToy) {
+    setToys( [...toys, newToy] )
+  }
+
+  useEffect( () => {
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(toys => setToys(toys)) // why do we need to set state to manage data coming in?
+  }, [])
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm addNewToy={addNewToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer addNewToy={addNewToy} toys={toys}/>
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ToyForm() {
+function ToyForm({addNewToy}) {
+
+  const [toyName, setToyName] = useState('')
+  const [toyImage, setToyImage] = useState('')
+
+  const handleToyName = (event) => setToyName(event.target.value)
+  const handleToyImage = (event) => setToyImage(event.target.value)
+
+  const handleNewToySubmit = (event) => {
+    event.preventDefault()
+    console.log('form clicked')
+
+    const OPTIONS = {
+      method: 'POST',
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ 
+        name: toyName,
+        image: toyImage, 
+        likes: 0
+      })
+    }
+
+    fetch('http://localhost:3000/toys', OPTIONS)
+    .then(res => res.json())
+    .then(newToy => addNewToy(newToy))
+
+
+    // resetForm()
+  }
+
   return (
     <div className="container">
       <form className="add-toy-form">
@@ -10,6 +42,8 @@ function ToyForm() {
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={toyName} // event listener steps: write the value + onChange for form
+          onChange={handleToyName}
         />
         <br />
         <input
@@ -17,6 +51,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={toyImage}
+          onChange={handleToyImage}
         />
         <br />
         <input
@@ -24,6 +60,7 @@ function ToyForm() {
           name="submit"
           value="Create New Toy"
           className="submit"
+          onClick={handleNewToySubmit}
         />
       </form>
     </div>
